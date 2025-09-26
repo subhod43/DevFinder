a0f500d572217c4c379223e9afbf4bf8be3d1624
Subhadeep Chakraborty <iamsubhad43@gmail.com>
2025-09-26 17:39:01 +0530

    chore: added basic components and routing

--- devFinder/src/App.jsx
+++ devFinder/src/App.jsx
@@ -1,38 +1,20 @@
 import React from 'react'
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Body from './Components/Body'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+const App = () => {
 
   return (
     <>
-      <div className="navbar bg-primary shadow-sm">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">👩‍💻 DevFinder</a>
-  </div>
-  <div className="flex gap-2">
-    <div className="dropdown dropdown-end mx-5">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-        </div>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
-      <h1>Dev Finder</h1>
-      
+        <BrowserRouter basename='/'>
+          <Routes>
+            <Route path='/' element={<Body />} >
+              <Route path='/login' element={<Login />} />
+              <Route path='/profile' element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
     </>
   )
 }
