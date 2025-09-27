8dbb18723c082c43b2aab0d61dfef683585f9d9b
Subhadeep Chakraborty <iamsubhad43@gmail.com>
2025-09-27 20:19:23 +0530

    chore: added axios and added login page card

--- devFinder/src/Components/Login.jsx
+++ devFinder/src/Components/Login.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  return (
+    <div className="flex justify-center my-48">
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset form-control">
+              <legend className="fieldset-legend">Email</legend>
+              <input
+                type="email"
+                className="input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </fieldset>
+            <fieldset className="fieldset form-control">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="password"
+                className="input"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center m-2">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
